26bcf1c860ef8c51c9c9fc34155aabd747efd60e
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2026-02-02 18:22:43 -0500

    feat: Created worksession4

---
+++ assignments/worksession4/scene.cpp
@@ -0,0 +1,136 @@
+#include "scene.h"
+
+// imgui
+#include "imgui/imgui.h"
+#include "imguizmo/imguizmo.h"
+
+// glm
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+// batteries
+#include "batteries/opengl.h"
+
+glm::mat4 lightMatrix = glm::mat4(1.0f);
+glm::vec3 lightColor = glm::vec3(1.0f);
+
+const glm::vec4 backgroundColor = glm::vec4(0.5f, 0.5f, 0.5f, 1.0f);
+
+struct{
+    float alpha = 128.0f;
+    bool isNormalMapOn = true;
+} debug;
+
+Scene::Scene()
+{
+    suzanne = std::make_unique<ew::Model>("assets/models/sponza.obj");
+
+    blinnphong = std::make_unique<ew::Shader>("assets/shaders/BlinnPhong.vs", "assets/shaders/BlinnPhong.fs");
+   
+    mainTexture = std::make_unique<ew::Texture>("assets/textures/Bricks.jpg");
+    normalTexture = std::make_unique<ew::Texture>("assets/textures/Bricks_Normal.jpg");
+
+    light = {
+        .brightness = 1.0f,
+        .color = {1.0f, 1.0f, 1.0f},
+        .position = {2.0f, 2.0f, 2.0f},
+    };
+
+    lightColor = light.color;
+}
+
+Scene::~Scene()
+{
+}
+
+void Scene::Update(float dt)
+{
+    batteries::Scene::Update(dt);
+
+    /* body */
+}
+
+auto objectMatrix = glm::mat4(1.0f);
+
+void Scene::Render(void)
+{
+    const auto view_proj = camera.Projection() * camera.View();
+
+    glClearColor(backgroundColor.x, backgroundColor.y, backgroundColor.z, backgroundColor.w);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glEnable(GL_DEPTH_TEST);
+    // glDisable(GL_DEPTH_TEST);
+
+    //Set main texture
+    glBindTextureUnit(0, mainTexture->getID());
+    //Set normal texture
+    glBindTextureUnit(1, normalTexture->getID());
+
+    blinnphong->use();
+
+    // scene matrices
+
+    blinnphong->setInt("main_texture", 0);
+    blinnphong->setInt("normal_map", 1);
+
+    blinnphong->setMat4("model", objectMatrix);
+    blinnphong->setMat4("view_proj", view_proj);
+    blinnphong->setVec3("camera", camera.position);
+
+    blinnphong->setVec3("light.position", light.position);
+    blinnphong->setVec3("light.color", light.color);
+    blinnphong->setFloat("material.shininess", debug.alpha);
+    blinnphong->setInt("normalMapOn", debug.isNormalMapOn);
+
+    blinnphong->setVec3("material.diffuse", glm::vec3(1));
+    blinnphong->setVec3("material.specular", glm::vec3(1));
+    blinnphong->setVec3("material.ambient", backgroundColor * 0.1f);
+
+    // draw suzanne
+    suzanne->draw();
+}
+
+void Scene::Debug(void)
+{
+    ImGuizmo::BeginFrame();
+    ImGuizmo::SetDrawlist(ImGui::GetBackgroundDrawList());
+    ImGuizmo::SetRect(0, 0, ImGui::GetIO().DisplaySize.x, ImGui::GetIO().DisplaySize.y);
+
+    glm::mat4 m{1.0f};
+    auto *view = glm::value_ptr(camera.View());
+    auto *proj = glm::value_ptr(camera.Projection());
+    
+    //ImGuizmo::DrawGrid(view, proj, glm::value_ptr(m), 100.0f);
+
+    light.color = lightColor;
+
+    if (ImGuizmo::IsUsing()){
+        light.position = glm::vec3(lightMatrix[3]);
+    }
+
+    ImGuizmo::Manipulate(
+        view,
+        proj,
+        ImGuizmo::TRANSLATE,
+        ImGuizmo::WORLD,
+        glm::value_ptr(lightMatrix) // &matrix[0][0]
+    );
+
+    cameracontroller.Debug();
+
+    ImGui::Begin("Controlls", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+
+    ImGui::SliderFloat("Alpha", &debug.alpha, 0, 128);
+    ImGui::Checkbox("Normal Mapping On", &debug.isNormalMapOn);
+    ImGui::ColorEdit3("Light Color", &lightColor.x);
+
+    ImGui::Checkbox("Paused", &time.paused);
+    ImGui::SliderFloat("Time Factor", &time.factor, 0.0f, 10.0f);
+
+    /* build debug ui here */
+
+    ImGui::End();
+}
